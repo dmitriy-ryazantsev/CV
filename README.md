e57220019bfd9feb4e0674098b89f114aa7cadb5
dmitriy-ryazantsev <dmitriy.ryazantsev@gmail.com>
2017-12-25 19:43:05 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# dmitriy-ryazantsev
+# Dmitriy Ryazantsev CV
+Run in cmd `npm run debug` and navigate in browser at `localhost:8081` url before editing source code.
---
+++ application.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import ReactDOM, { render } from 'react-dom';
+
+// class Application extends React.Component
+// {
+//     render()
+//     {
+//         return (<div>Hellow world!</div>);
+//     }
+// }
+
+// ReactDOM.render(
+//   <Application/>,
+//   document.getElementById('application-content')
+// );
